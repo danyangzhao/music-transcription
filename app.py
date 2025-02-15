371915b785c42c8e1cff37d1411861d56eb3e078
Dan Zhao <7462922+danyangzhao@users.noreply.github.com>
2025-02-15 12:06:16 -0800

    added Flask app

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
-//sudo code
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    # debug=True enables auto-reloading of the server when you make code changes
+    app.run(debug=True)
 
